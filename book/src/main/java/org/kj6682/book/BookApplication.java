eeca02467394cc8a3bd537f7bd897e0b00890142
KJ6682 <luigi.mastroserio@gmail.com>
2025-01-11 17:25:33 +0100

    remember that resources utf-8 need filtering on mvn resource plugin

--- book/src/main/java/org/kj6682/book/BookApplication.java
+++ book/src/main/java/org/kj6682/book/BookApplication.java
@@ -4,11 +4,18 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.List;
+import java.util.Locale;
 
 @SpringBootApplication
-public class BookApplication {
+public class BookApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
@@ -32,5 +39,23 @@ public class BookApplication {
     @Bean
     public CustomizedErrorAttributes errorAttributes(){
         return new CustomizedErrorAttributes();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ITALIAN);
+        return slr;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
